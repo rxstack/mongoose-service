46110b929838081143cec14b424e07f31bd25430
Nikolay Georgiev <n.georgiev@testbirds.de>
2019-01-25 17:59:53 +0200

    added classes to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './interfaces';
 export * from './mongoose-service';
 export * from './mongoose-service.module';
+export * from './mongoose-purger';
+export * from './mongoose-user.provider';
